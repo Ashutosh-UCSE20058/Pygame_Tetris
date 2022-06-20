5ddbfd9ad175aa54d51e957367293cd3d7cb881f
Ashutosh-UCSE20058 <82892946+Ashutosh-UCSE20058@users.noreply.github.com>
2022-06-20 12:48:29 +0530

    Tetris main.py added

---
+++ Tetris/main.py
@@ -0,0 +1,15 @@
+
+# This main file is to execute the game file named tetris.py
+# Importing the game file and the os module for running the game
+
+# HOW TO PLAY THE TETRIS GAME
+# Step-1: Click on any button to start the game
+# Step-2: The game is played using the Arrow's Keys
+# Step-3: Left Arrow Key is used to make the shape go left.
+# Step-4: Right Arrow Key is used to make the shape go right.
+# Step-5: Upward Arrow Key is used to change the direction the shape is current in.
+
+import tetris.py
+import os
+if __name__=="__main":
+    os.system('python tetr.py')
---
+++ Tetris/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+win = pygame.display.set_mode((s_width, s_height))
+pygame.display.set_caption('Tetris')
+ 
+main_menu() 
